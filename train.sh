29256e200f1657e977f0bed73cb56d8984a1f364
khangtruong-ui <khang.truong@gmail.com>
2026-02-06 14:05:50 +0700

    Update train.sh

--- train.sh
+++ train.sh
@@ -2,9 +2,9 @@ python train.py \
     --pretrained_model_name_or_path="CompVis/stable-diffusion-v1-4"  \
     --dataset_name="KhangTruong/NWPU_Split" \
     --resolution=256 --center_crop --random_flip \
-    --train_batch_size=64 \
+    --train_batch_size=128 \
     --num_train_epochs=1000 \
     --learning_rate=1e-05 \
     --max_grad_norm=1 \
-    --revision="flax"\
+    --revision="flax" \
     --output_dir="./sd-full-finetuned" \
