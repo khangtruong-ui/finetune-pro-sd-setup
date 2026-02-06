28b76e18c4bda751ee77424ed219c0e5f5e289ab
khangtruong-ui <khang.truong@gmail.com>
2026-02-06 15:30:37 +0700

    Update dataset.py

--- data/dataset.py
+++ data/dataset.py
@@ -100,7 +100,7 @@ def get_dataloader(config, tokenizer):
         num_workers=4
      )
 
-    next(torch_loader)
+    next(iter(torch_loader))
 
     loader_length = len(dataset) // (config.train_batch_size * jax.local_device_count())
     return {'loader': torch_loader, 'length': loader_length}
